c6fe6cf90d209d08f212b0e64cb09dc15bee5582
Vulpes231 <whykayshot@gmail.com>
2023-09-04 20:43:09 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const connectWallet = document.getElementById("connect-wallet");
+  const getBalance = document.getElementById("get-bal");
+  const fundContract = document.getElementById("fund");
+  const withdrawFromContract = document.getElementById("withdraw");
+  const myInput = document.getElementById("my-input");
+
+  let val;
+
+  connectWallet.addEventListener("click", function (e) {
+    e.preventDefault();
+    console.log("Connect wallet clicked");
+  });
+
+  getBalance.addEventListener("click", function (e) {
+    e.preventDefault();
+    val = myInput.value;
+    console.log(!val ? 0 : val, "Get balance clicked");
+    // console.log(val);
+  });
+  fundContract.addEventListener("click", function (e) {
+    e.preventDefault();
+    val = myInput.value;
+    console.log(!val ? 0 : val, "Fund contract clicked");
+  });
+  withdrawFromContract.addEventListener("click", function (e) {
+    e.preventDefault();
+    val = myInput.value;
+    console.log(!val ? 0 : val, "Withdraw  clicked");
+  });
+});
